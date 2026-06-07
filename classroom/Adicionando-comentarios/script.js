fdab2d3f98609462a1bed7d1c9598b23d8105960
THIAGO FIGUEIREDO FERREIRA <thiagofigueiredoferrera@gmail.com>
2026-06-07 14:21:44 -0300

    Conclui estudos de introdução ao JavaScript

---
+++ classroom/Adicionando-comentarios/script.js
@@ -0,0 +1,8 @@
+// O console.log exibe no console um Texto.
+
+/*
+- Este é um comentário
+- de várias linhas
+*/
+
+console.log("Thiago")
---
+++ classroom/sintaxe/script.js
@@ -0,0 +1,10 @@
+// Sintaxe correta
+console.log("Olá, Thiago!")
+
+//Sintaxe incorreta
+// cons.ole.log("Olá, Thiago!")
+
+
+// No Javascript ponto e virgula é opicional.
+console.log("Olá, Thiago");
+console.log("Olá, Thiago")
