eb1654fa8133b766f1f1199b23473e577cc14e47
THIAGO FIGUEIREDO FERREIRA <thiagofigueiredoferrera@gmail.com>
2026-06-11 21:42:56 -0300

    feat: aplicar boas práticas na nomeação de variáveis

---
+++ rocketseat/nome-para-variáveis/script.js
@@ -0,0 +1,40 @@
+
+
+//  - ESCREVER NOMES DAS VARIÁVEIS EM INGLES
+
+
+// - case-sensitive
+let username = "rodrigo"
+let userName = "joão"
+
+console.log(username)
+console.log(userName)
+
+//PODEMOS
+let $email = "rodrigo@gmail.com"
+let _email = "thiago@gmail.com"
+let user_email = "maria@gmail.com"
+let ação = "cadastro" // não é recomendado usar caracteres especiais.
+
+console.log($email)
+console.log(_email)
+console.log(user_email)
+console.log(ação) // não é recomendado usar caracteres especiais.
+
+//Nao podemos usar numeros.
+// let 1user = "thiago"
+// var product name = "luva"
+
+//recomendação camelcase  (corcunda do camelo)
+
+// Sempre usar nomes que tenham a ver com o conteudo.
+
+let productName = "Teclado"
+let firstName = "thiago"
+let lastname = "figueira"
+
+//Recomendação snake_case (rastejar)
+
+let product_name = "Teclado"
+let first_name = "thiago"
+let last_name = "Figueira"
