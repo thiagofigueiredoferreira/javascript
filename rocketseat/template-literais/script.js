549a313c369df6a33c68651ec26b4ac8a6d02cba
THIAGO FIGUEIREDO FERREIRA <thiagofigueiredoferrera@gmail.com>
2026-06-11 21:43:49 -0300

    feat: utilizar template literals para interpolação de texto

---
+++ rocketseat/template-literais/script.js
@@ -0,0 +1,14 @@
+// Template literais (template string, interpolação de string)
+let username = "thiago"
+let email = "thiago@gmail.com"
+
+// passar mais de um parãmetro.
+console.log(username, email, "teste")
+
+//concatenar texto.
+let messagem = "Olá, " + username + ". Você conectou com o e-mail " + email
+
+console.log(messagem)
+
+//Template literals
+console.log(`Olá, ${username}. Você conectou com o e-mail ${email}`)
