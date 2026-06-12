fe8397595aeeafd5b34bc3bec9dd1c0b4ea22cc1
THIAGO FIGUEIREDO FERREIRA <thiagofigueiredoferrera@gmail.com>
2026-06-11 21:43:11 -0300

    feat: explorar o tipo number e operações numéricas

---
+++ rocketseat/number/script.js
@@ -0,0 +1,14 @@
+//Number
+console.log( typeof 5)
+
+//Inteiro positivo.
+console.log(5)
+
+//Inteiro negativo.
+console.log(-5)
+
+//Number reais ou float.
+console.log(125.79)
+
+//NaN - Not a Number.
+console.log(12.5 / "thiago")
