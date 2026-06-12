c4920d54aa808bd93dc586a1d69951558782afe9
THIAGO FIGUEIREDO FERREIRA <thiagofigueiredoferrera@gmail.com>
2026-06-11 21:43:37 -0300

    feat: praticar manipulação de strings em JavaScript

---
+++ rocketseat/string/script.js
@@ -0,0 +1,13 @@
+// string
+let username = "thaigo"
+console.log(username)
+console.log(typeof username)
+
+console.log("Uma string com aspas")
+console.log('Uma string com apóstrofo (aspas simples)')
+
+// Quando ultilizar uma ou outras?
+console.log('Uma string com "aspas duplas" dentro de simples.')
+console.log("Uma string com 'aspas simples' dentro de dupla")
+console.log(`Uma string com acento grave permite
+escrever múltiplas linhas.`)
