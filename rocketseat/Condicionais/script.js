891d07a1ffb9724745d34e6139c4bb8a76c5836d
THIAGO FIGUEIREDO FERREIRA <thiagofigueiredoferrera@gmail.com>
2026-06-16 19:45:59 -0300

    feat: aprender operador condicional ternário

---
+++ rocketseat/Condicionais/script.js
@@ -0,0 +1,14 @@
+// Operador condicional ternário
+
+let age = 21
+console.log(
+    age >= 18 // condição
+     ? "Você pode dirigir" // se verdadeira
+    :"Você não pode dirigir." //se falso
+)
+
+console.log(
+    age < 18
+    ? "Você não pode dirigir."
+    : "Você pode dirigir."
+)
