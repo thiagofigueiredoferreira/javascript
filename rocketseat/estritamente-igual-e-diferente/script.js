43ad03bef5c65ef359400c639aa3c07840a62bcb
THIAGO FIGUEIREDO FERREIRA <thiagofigueiredoferrera@gmail.com>
2026-06-16 13:04:44 -0300

    feat: aprender operadores estritamente igual e diferente

---
+++ rocketseat/estritamente-igual-e-diferente/script.js
@@ -0,0 +1,14 @@
+let one = 1
+let two = 2
+
+// Estritamente igual a(tipo e valor)
+// Sempre comparar dessa forma, melhor forma de comparar um dado, tipo e o valor.
+
+console.log(one === 1)
+console.log(one === "1")
+
+// !== estritamemte diferente de 
+console.log(one !== two)
+console.log(one !== 1)
+console.log(two !== 2)
+console.log(two !== "2")
