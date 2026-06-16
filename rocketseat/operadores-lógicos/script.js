5120ea4f341efcc477f26ca8faa9544da344e8f3
THIAGO FIGUEIREDO FERREIRA <thiagofigueiredoferrera@gmail.com>
2026-06-16 13:08:05 -0300

    feat: praticar operadores lógicos

---
+++ rocketseat/operadores-lógicos/script.js
@@ -0,0 +1,11 @@
+let email = true
+let password = false
+
+// AND (E) &&
+console.log(email && password)
+
+// OR (OU) ||
+console.log(email || password)
+
+//NOT (NEGAÇÃO) !
+console.log(!password)
