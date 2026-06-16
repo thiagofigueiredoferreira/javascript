bbed248a856c011691c4189f57fcc741d228e4c5
THIAGO FIGUEIREDO FERREIRA <thiagofigueiredoferrera@gmail.com>
2026-06-16 13:05:31 -0300

    feat: praticar operadores de igualdade e diferença

---
+++ rocketseat/igual-a-e-diferente-de/script.js
@@ -0,0 +1,16 @@
+let one = 1
+let two = 2
+
+// == igual a 
+
+console.log("## IGUAL A ##")
+console.log(one == two)
+console.log(one == 1)
+console.log(one == "1")
+
+// != diferente de 
+
+console.log("## DIFERENTE DE ##")
+console.log(one != two)
+console.log(one != 1)
+console.log(one != "1")
