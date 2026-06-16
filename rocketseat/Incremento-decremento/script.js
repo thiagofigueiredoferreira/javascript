0c01dfc4298815223a13dd3e7f56fb4a70fb6223
THIAGO FIGUEIREDO FERREIRA <thiagofigueiredoferrera@gmail.com>
2026-06-16 13:05:51 -0300

    feat: adicionar exemplos de incremento e decremento

---
+++ rocketseat/Incremento-decremento/script.js
@@ -0,0 +1,26 @@
+let number = 10
+
+// INCREMENTO
+
+ // number = number + 1
+ number++
+
+ // Incrementa após (por isso não mostra no console)
+console.log(number++)
+console.log(number)
+
+// Incrementa antes
+console.log(++number)
+
+//Decremento
+console.log("Decremento após: ", number--)
+console.log(number--)
+console.log("Decremento antes: ", --number)
+
+//incrementar mais de um (COLOCAR).
+number += 10
+console.log(number)
+
+//incrementar mais de um (REMOVENDO).
+number -= 5
+console.log(number)
