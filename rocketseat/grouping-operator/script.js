eced0de3f9e6d15aac774465c035c31376255ad7
THIAGO FIGUEIREDO FERREIRA <thiagofigueiredoferrera@gmail.com>
2026-06-16 13:05:08 -0300

    feat: estudar operador de agrupamento

---
+++ rocketseat/grouping-operator/script.js
@@ -0,0 +1,13 @@
+// Grouping Operator (ordem de precedência)
+
+let total1 = 2 + 3 * 4
+console.log(total1)
+
+let total2 = (2 + 3) * 4
+console.log(total2)
+
+let average = (9.5 + 7 + 5) / 3
+console.log(average)
+
+let resultado = (500 + 185) / 5
+console.log(resultado)
