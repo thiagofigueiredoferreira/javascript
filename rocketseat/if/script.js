09c78e613223979c551c2ebca4ba92e63e5d0730
THIAGO FIGUEIREDO FERREIRA <thiagofigueiredoferrera@gmail.com>
2026-06-21 14:14:43 -0300

    feat: adicionando exemplos de uso da estrutura condicional if

---
+++ rocketseat/if/script.js
@@ -0,0 +1,16 @@
+// IF (SE)
+
+let hour = 11
+
+if (hour <= 13) {
+    console.log("Bom dia!")
+    console.log("Seja bem vindo!")
+
+}
+
+if (hour <= 12)
+    console.log("Bom dia!")
+    console.log("Seja bem vindo")
+
+
+    //Duas maneiras de representar o if
