5113b7b6b4a2ddc9d9417da1ede82bb03f402224
THIAGO FIGUEIREDO FERREIRA <thiagofigueiredoferrera@gmail.com>
2026-06-21 20:40:32 -0300

    feat: explora valores truthy e falsy no JavaScript

---
+++ rocketseat/falsy-e-truthy/script.js
@@ -0,0 +1,27 @@
+/*
+FALSY  quando um vaçor é considerado false e TRUTHY quando é 
+considerado verdadeiro em contextos onde um boolean é obrigatório
+(condicionais e loops)
+*/
+
+console.log("### EXEMPLOS DE FALSY ###")
+console.log(false ? "VERDADEIRO" : "FALSO") 
+console.log(0 ? "VERDADEIRO" : "FALSO")
+console.log(-0 ? "VERDADEIRO" : "FALSO")
+console.log( ""? "VERDADEIRO" : "FALSO")
+console.log(null ? "VERDADEIRO" : "FALSO")
+console.log(undefined ? "VERDADEIRO" : "FALSO")
+console.log(NaN ? "VERDADEIRO" : "FALSO")
+
+console.log("### EXEMPLOS DE TRUTHY ###")
+console.log(true ? "VERDADEIRO" : "FALSO")
+console.log({} ? "VERDADEIRO" : "FALSO")
+console.log([] ? "VERDADEIRO" : "FALSO")
+console.log(1 ? "VERDADEIRO" : "FALSO")
+console.log(3.23 ? "VERDADEIRO" : "FALSO")
+console.log("0" ? "VERDADEIRO" : "FALSO")
+console.log(" " ? "VERDADEIRO" : "FALSO")
+console.log("Rodrigo" ? "VERDADEIRO" : "FALSO")
+console.log("false" ? "VERDADEIRO" : "FALSO")
+console.log("-1" ? "VERDADEIRO" : "FALSO")
+console.log(Infinity ? "VERDADEIRO" : "FALSO")
