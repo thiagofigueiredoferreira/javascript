b7182547a5494c4cfb046fdb7ba07951e1a74bea
THIAGO FIGUEIREDO FERREIRA <thiagofigueiredoferrera@gmail.com>
2026-06-21 20:41:16 -0300

    geat: implementa exemplos de controlede fluxo com switch

---
+++ rocketseat/switch/script.js
@@ -0,0 +1,16 @@
+let option = 1
+
+switch (option){
+    case 1:
+        console.log("Consultar pedido")
+        console.log("Aguarde...")
+        break
+    case 2:
+        console.log("Falar com o atendente")    
+        break
+    case 3:
+        console.log("Cancelar pedido")  
+        break
+         default:
+            console.log("Opção inválida!")
+}
