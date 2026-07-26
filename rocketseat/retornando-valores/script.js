40c060c34db7631afb01d7450cafac7a1decd338
THIAGO FIGUEIREDO FERREIRA <thiagofigueiredoferrera@gmail.com>
2026-07-26 19:18:39 -0300

    "feat: implementa exemplos de retorno de valores"

---
+++ rocketseat/retornando-valores/script.js
@@ -0,0 +1,15 @@
+function sum(a,b){
+    let result = a + b
+    console.log(result)
+}
+sum(7,3)
+function sus(a,b){
+    let resultado = a + b
+
+     return resultado
+}
+
+let response = sus(7,7)
+console.log(response)
+
+console.log(sus(5,6))
