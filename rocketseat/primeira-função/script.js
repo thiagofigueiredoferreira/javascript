9bbfdb8f0e330df30da2ae91569ab5ed4948e13d
THIAGO FIGUEIREDO FERREIRA <thiagofigueiredoferrera@gmail.com>
2026-07-26 19:18:14 -0300

    "feat: conclui aula de primeira função"

---
+++ rocketseat/primeira-função/script.js
@@ -0,0 +1,9 @@
+function message(){
+    console.log("Olá. é bom te ver aqui!")
+}
+
+message()
+
+// função tem que ter um nome para poder ser chamada, e um valor dentro dela para poder exibir ou rodar o codigo.
+
+// posso chamar ela quantas vezes eu quiser, reaproveitando o conteudo dela.
