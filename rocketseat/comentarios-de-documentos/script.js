e8d6fbc9d9ad471a992aa7db8e745ef5235b5e7c
THIAGO FIGUEIREDO FERREIRA <thiagofigueiredoferrera@gmail.com>
2026-07-28 23:12:31 -0300

    docs: adiciona documentação JSDoc na função signIn

---
+++ rocketseat/comentarios-de-documentos/script.js
@@ -0,0 +1,22 @@
+//a documentar funções em JavaScript usando JSDoc, deixando o código mais organizado, fácil de entender e mais profissional.
+
+/*
+ Comentario de documentação em JavaScript (sintaxe de JSDoc).
+ o JSDoc é um padão para incorporar documentação no codigo-fonte a
+ partir desses comentários.
+ */
+
+/** 
+ * Authenticates the user
+ * 
+ * @param {String} email user email.
+ * @param {String} password more than 6 characters.
+ * @returns {Number} user id.
+*/
+ function signIn(email, password){
+    // Fluxo de autenticação do usuário.
+
+    return 7 
+ }
+
+ signIn("thiago@gmail.com","1234")
