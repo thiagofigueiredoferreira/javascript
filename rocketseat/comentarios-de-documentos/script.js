fc32f826d72531e788b32e029144075f77cda759
THIAGO FIGUEIREDO FERREIRA <thiagofigueiredoferrera@gmail.com>
2026-07-31 11:35:14 -0300

    docs: aprofunda estudo sobre comentários JSDoc

--- rocketseat/comentarios-de-documentos/script.js
+++ rocketseat/comentarios-de-documentos/script.js
@@ -5,6 +5,8 @@
  o JSDoc é um padão para incorporar documentação no codigo-fonte a
  partir desses comentários.
  */
+/*-------------------------*/
+
 
 /** 
  * Authenticates the user
@@ -13,10 +15,38 @@
  * @param {String} password more than 6 characters.
  * @returns {Number} user id.
 */
+
  function signIn(email, password){
     // Fluxo de autenticação do usuário.
 
     return 7 
  }
 
- signIn("thiago@gmail.com","1234")
+ signIn("thiago@gmail.com", " 1234")
+
+
+
+//  function hero(horoi, vilão){
+
+  
+//    hero("home-aranha", "coringa")
+ 
+//   return "parabens"
+//  }
+
+//  console.log(hero("home-aranha", "coringa"))
+
+function hero(heroi, vilao) {
+  return `Parabéns, ${heroi}! Você venceu o ${vilao}.`
+}
+
+console.log(hero("homem-aranha", "coringa"))
+
+//A ideia da função é:
+
+// receber dois parâmetros;
+// usar esses parâmetros;
+// retornar algo;
+// chamar a função fora dela.
+
+ 
