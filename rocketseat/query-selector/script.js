109f56c69f3081bdb899fb5106ea306ac634ec86
THIAGO FIGUEIREDO FERREIRA <thiagofigueiredoferrera@gmail.com>
2026-08-08 14:21:09 -0300

    fix: corrige referências e seletores do querySelector

--- rocketseat/query-selector/script.js
+++ rocketseat/query-selector/script.js
@@ -1,2 +1,12 @@
+//Acessa o elemento pelo selector ID
 const guest = document.querySelector("#guest-1")
-console.log("guest")
+console.log(guest)
+
+//Acessa o elemento pelo selector class
+// const guests = document.querySelector(".guest")
+
+
+//Retorna todos os elementos encontrados pela query
+const guests = document.querySelectorAll(".guest")
+
+console.log(guests)
