edfbad13378542f7bd2bd27a777ff2c9532515cf
THIAGO FIGUEIREDO FERREIRA <thiagofigueiredoferrera@gmail.com>
2026-08-11 14:20:05 -0300

    feat(dom): adiciona manipulacao de classes e estilos via javascript

---
+++ rocketseat/alterando-estilos/script.js
@@ -0,0 +1,15 @@
+ const input = document.querySelector("#name")
+ //Adiciona a class.
+ //input.classList.add("input-error")
+
+ //Remover a classe.
+ //input.classList.remove("input-error")
+
+
+ //Se não tiver a clsse adiciona. se tem, remove.
+// input.classList.toggle("input-error")
+
+const button = document.querySelector("button")
+
+// Modificar as propriedades css do elemento. 
+ button.style.backgroundColor = "red" 
