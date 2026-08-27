af3b95c65dea04296cc81a383839d3c80dd5d95e
THIAGO FIGUEIREDO FERREIRA <thiagofigueiredoferrera@gmail.com>
2026-08-27 14:12:40 -0300

    feat: Adicionando anotações

--- rocketseat/acessando-elementos-no-DOM/scripts.js
+++ rocketseat/acessando-elementos-no-DOM/scripts.js
@@ -1,24 +1,51 @@
-// Visualizar o conteúdo do documnt
-//console.log(document)
+// // Visualizar o conteúdo do documnt
+// //console.log(document)
 
-//Obter o title da pagina
+// //Obter o title da pagina
+// console.log(document.title)
+
+// // Acessar o elemento pelo ID (SELETOR ID)
+// const guest = document.getElementById("guest-2") // getElementById = consigo acessar elementos pelo id
+// console.log(guest)
+
+// //Mostra a propriedade do objetos.
+// console.dir(guest) // consutando as propriedades dos elemento com (DIR)
+
+// //Acessar elemento com class (SELETOR class)
+// const guestsByClass = document.getElementsByClassName("guest")
+// console.log(guestsByClass)
+
+
+// //Exibir o primeiro elemento da lista.
+// console.log(guestsByClass.item(0)) // Sempre começa pelo 0
+// console.log(guestsByClass[1]) // Vai do mesmo jeito de cima 
+
+// // Selecionar por lista de elementos pela tag
+//  const  guestsbyTag = document.getElementsByTagName("li")
+//  console.log(guestsbyTag)
+
+
 console.log(document.title)
 
-//Acessar o elemento pelo ID (SELETOR ID)
+
 const guest = document.getElementById("guest-2")
 console.log(guest)
-
-//Mostra a propriedade do objetos.
 console.dir(guest)
 
-//Acessar elemento com class (SELETOR class)
-const guestByClass = document.getElementsByClassName("guest")
-console.log(guestByClass)
+const guests = document.getElementsByClassName("guest")
+console.log(guests)
 
-//Exibir o primeiro elemento da lista.
-console.log(guestByClass.item(0)) // Sempre começa pelo 0
-console.log(guestByClass[1]) // Vai do mesmo jeito de cima 
+console.log(guests.item(0))
+console.log(guests[3])
 
-// Selecionar por lista de elementos pela tag
-const  guestsbyTag = document.getElementsByTagName("li")
-console.log(guestsbyTag)
+// const nome = document.getElementsByTagName("li")
+// console.log(nome)
+
+const nome = document.getElementsByTagName("li")
+console.log(nome)
+ 
+
+
+
+
+
