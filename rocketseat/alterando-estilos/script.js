e876b926625ae843e7d1fd899f4dd21a3c8bbe7e
THIAGO FIGUEIREDO FERREIRA <thiagofigueiredoferrera@gmail.com>
2026-09-03 14:23:14 -0300

    feat: pratica de classList e style no DOM

--- rocketseat/alterando-estilos/script.js
+++ rocketseat/alterando-estilos/script.js
@@ -1,18 +1,23 @@
  const input = document.querySelector("#name")
+
+// CRIANDO UMA CLASS COM JS
  //Adiciona a class.
- //input.classList.add("input-error")
+ //add : adiciona class
+ input.classList.add("input-error")
 
  //Remover a classe.
- //input.classList.remove("input-error")
+ input.classList.remove("input-error")
 
 
  //Se não tiver a clsse adiciona. se tem, remove.
-// input.classList.toggle("input-error")
+ input.classList.toggle("input-error")
 
 const button = document.querySelector("button")
 
+//USANDO CSS NO JS 
 // Modificar as propriedades css do elemento. 
  button.style.backgroundColor = "red" ; 
+
 
 
 
